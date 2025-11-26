3e79ea107a42028c47afb85821565afbd3ea529a
Shwerch <160221979+Shwerch@users.noreply.github.com>
2025-11-27 04:21:51 +0700

    DoubleLinkedList ready

--- C++/include/doubly_linked_list.h
+++ C++/include/doubly_linked_list.h
@@ -7,52 +7,57 @@
 #include <cstring>
 
 template<typename T>
-struct Node {
+struct DNode {
     T value;
-    Node* next;
-    explicit Node(const T& value) : value(value), next(nullptr) {}
+    DNode* next;
+    DNode* prev;
+    explicit DNode(const T& value) : value(value), next(nullptr), prev(nullptr) {}
 };
 
 template<typename T>
-struct LinkedList final : Base {
+struct DoublyLinkedList final : Base {
 private:
-    Node<T>* head_ = nullptr;
+    DNode<T>* head_ = nullptr;
+    DNode<T>* tail_ = nullptr;
     size_t size_ = 0;
 
 public:
-    LinkedList() = default;
+    DoublyLinkedList() = default;
 
-    ~LinkedList() override {
-        Node<T>* current = head_;
+    ~DoublyLinkedList() override {
+        DNode<T>* current = head_;
         while (current != nullptr) {
-            Node<T>* next_node = current->next;
+            DNode<T>* next_node = current->next;
             delete current;
             current = next_node;
         }
     }
 
-    LinkedList(const LinkedList& other) : Base(other) {
+    DoublyLinkedList(const DoublyLinkedList& other) : Base(other) {
         if (other.head_ == nullptr) {
             return;
         }
 
-        head_ = new Node<T>(other.head_->value);
-        Node<T>* current_new = head_;
-        Node<T>* current_old = other.head_->next;
+        head_ = new DNode<T>(other.head_->value);
+        DNode<T>* current_new = head_;
+        DNode<T>* current_old = other.head_->next;
 
         while (current_old != nullptr) {
-            current_new->next = new Node<T>(current_old->value);
+            current_new->next = new DNode<T>(current_old->value);
+            current_new->next->prev = current_new;
             current_new = current_new->next;
             current_old = current_old->next;
         }
 
+        tail_ = current_new;
         size_ = other.size_;
     }
 
-    LinkedList(LinkedList&& other) noexcept
-            : Base(std::move(other)), head_(other.head_), size_(other.size_)
+    DoublyLinkedList(DoublyLinkedList&& other) noexcept
+            : Base(std::move(other)), head_(other.head_), tail_(other.tail_), size_(other.size_)
     {
         other.head_ = nullptr;
+        other.tail_ = nullptr;
         other.size_ = 0;
     }
 
@@ -61,99 +66,107 @@ public:
     }
 
     void push_before(const T& value, const T& new_value) {
-        Node<T>* prev_node = nullptr;
-        Node<T>* current_node = head_;
+        DNode<T>* current_node = head_;
         while (current_node != nullptr && current_node->value != value) {
-            prev_node = current_node;
             current_node = current_node->next;
         }
         if (current_node == nullptr) {
             throw std::runtime_error("Element not found");
         }
-        Node<T>* new_node = new Node(new_value);
-        if (prev_node == nullptr) {
-            new_node->next = head_;
-            head_ = new_node;
+        DNode<T>* new_node = new DNode<T>(new_value);
+        new_node->next = current_node;
+        new_node->prev = current_node->prev;
+        
+        if (current_node->prev != nullptr) {
+            current_node->prev->next = new_node;
         } else {
-            prev_node->next = new_node;
-            new_node->next = current_node;
+            head_ = new_node;
         }
+        current_node->prev = new_node;
         ++size_;
     }
 
     void push_after(const T& value, const T& new_value) {
-        Node<T>* current_node = head_;
+        DNode<T>* current_node = head_;
         while (current_node != nullptr && current_node->value != value) {
             current_node = current_node->next;
         }
         if (current_node == nullptr) {
             throw std::runtime_error("Element not found");
         }
-        Node<T>* new_node = new Node(new_value);
+        DNode<T>* new_node = new DNode<T>(new_value);
+        new_node->prev = current_node;
         new_node->next = current_node->next;
+        
+        if (current_node->next != nullptr) {
+            current_node->next->prev = new_node;
+        } else {
+            tail_ = new_node;
+        }
         current_node->next = new_node;
         ++size_;
     }
 
     void push_head(const T& new_value) {
-        Node<T>* new_node = new Node(new_value);
-        if (size() > 0) {
+        DNode<T>* new_node = new DNode<T>(new_value);
+        if (size_ == 0) {
+            head_ = tail_ = new_node;
+        } else {
             new_node->next = head_;
+            head_->prev = new_node;
+            head_ = new_node;
         }
-        head_ = new_node;
         ++size_;
     }
 
     void push_tail(const T& new_value) {
-        Node<T>* new_node = new Node(new_value);
-        if (size() == 0) {
-            head_ = new_node;
+        DNode<T>* new_node = new DNode<T>(new_value);
+        if (size_ == 0) {
+            head_ = tail_ = new_node;
         } else {
-            Node<T>* current_node = head_;
-            while (current_node->next != nullptr) {
-                current_node = current_node->next;
-            }
-            current_node->next = new_node;
+            new_node->prev = tail_;
+            tail_->next = new_node;
+            tail_ = new_node;
         }
         ++size_;
     }
 
-    const Node<T>* get_head() const {
+    const DNode<T>* get_head() const {
         if (size_ == 0) {
             throw std::runtime_error("Cannot get head from empty list");
         }
         return head_;
     }
 
-    const Node<T>* get_tail() const {
+    const DNode<T>* get_tail() const {
         if (size_ == 0) {
             throw std::runtime_error("Cannot get tail from empty list");
         }
-        Node<T>* current_node = head_;
-        while (current_node->next != nullptr) {
-            current_node = current_node->next;
-        }
-        return current_node;
+        return tail_;
     }
 
     void remove(const T& value) {
-        Node<T>* prev_node = nullptr;
-        Node<T>* current_node = head_;
+        DNode<T>* current_node = head_;
         while (current_node != nullptr && current_node->value != value) {
-            prev_node = current_node;
             current_node = current_node->next;
         }
         if (current_node == nullptr) {
             throw std::runtime_error("Element not found");
         }
-        if (prev_node == nullptr) {
-            Node<T>* node_to_delete = head_;
-            head_ = head_->next;
-            delete node_to_delete;
+        
+        if (current_node->prev != nullptr) {
+            current_node->prev->next = current_node->next;
         } else {
-            prev_node->next = current_node->next;
-            delete current_node;
+            head_ = current_node->next;
         }
+        
+        if (current_node->next != nullptr) {
+            current_node->next->prev = current_node->prev;
+        } else {
+            tail_ = current_node->prev;
+        }
+        
+        delete current_node;
         --size_;
     }
 
@@ -161,23 +174,23 @@ public:
         if (size_ < 2 || head_->value == value) {
             throw std::runtime_error("Cannot remove element before list head or list is too small");
         }
-        Node<T>* prev_prev_node = nullptr;
-        Node<T>* prev_node = head_;
-        Node<T>* current_node = head_->next;
+        DNode<T>* current_node = head_;
         while (current_node != nullptr && current_node->value != value) {
-            prev_prev_node = prev_node;
-            prev_node = current_node;
             current_node = current_node->next;
         }
         if (current_node == nullptr) {
             throw std::runtime_error("Element with given value not found");
         }
-        if (prev_prev_node == nullptr) {
-            head_ = current_node;
+        
+        DNode<T>* node_to_delete = current_node->prev;
+        if (node_to_delete->prev != nullptr) {
+            node_to_delete->prev->next = current_node;
         } else {
-            prev_prev_node->next = current_node;
+            head_ = current_node;
         }
-        delete prev_node;
+        current_node->prev = node_to_delete->prev;
+        
+        delete node_to_delete;
         --size_;
     }
 
@@ -185,7 +198,7 @@ public:
         if (size_ < 2) {
             throw std::runtime_error("List is too small");
         }
-        Node<T>* current_node = head_;
+        DNode<T>* current_node = head_;
         while (current_node != nullptr && current_node->value != value) {
             current_node = current_node->next;
         }
@@ -195,9 +208,16 @@ public:
         if (current_node->next == nullptr) {
             throw std::runtime_error("No node to delete");
         }
-        auto delete_node = current_node->next;
-        current_node->next = current_node->next->next;
-        delete delete_node;
+        
+        DNode<T>* node_to_delete = current_node->next;
+        current_node->next = node_to_delete->next;
+        if (node_to_delete->next != nullptr) {
+            node_to_delete->next->prev = current_node;
+        } else {
+            tail_ = current_node;
+        }
+        
+        delete node_to_delete;
         --size_;
     }
 
@@ -205,9 +225,14 @@ public:
         if (size_ == 0) {
             throw std::runtime_error("List is too small");
         }
-        auto delete_node = head_;
+        DNode<T>* node_to_delete = head_;
         head_ = head_->next;
-        delete delete_node;
+        if (head_ != nullptr) {
+            head_->prev = nullptr;
+        } else {
+            tail_ = nullptr;
+        }
+        delete node_to_delete;
         --size_;
     }
 
@@ -215,24 +240,19 @@ public:
         if (size_ == 0) {
             throw std::runtime_error("Cannot get tail from empty list");
         }
-        if (size_ == 1) {
-            delete head_;
-            head_ = nullptr;
+        DNode<T>* node_to_delete = tail_;
+        tail_ = tail_->prev;
+        if (tail_ != nullptr) {
+            tail_->next = nullptr;
         } else {
-            Node<T>* prev_node = head_;
-            Node<T>* current_node = head_->next;
-            while (current_node->next != nullptr) {
-                prev_node = current_node;
-                current_node = current_node->next;
-            }
-            prev_node->next = nullptr;
-            delete current_node;
+            head_ = nullptr;
         }
+        delete node_to_delete;
         --size_;
     }
 
     bool contains(const T& value) {
-        Node<T>* current_node = head_;
+        DNode<T>* current_node = head_;
         while (current_node != nullptr) {
             if (current_node->value == value) {
                 return true;
@@ -245,52 +265,52 @@ public:
 };
 
 template<typename T>
-struct LinkedListHelper final : Helper {
+struct DoublyLinkedListHelper final : Helper {
 private:
-    static LinkedList<T>* cast(Base* b) {
-        auto* ptr = dynamic_cast<LinkedList<T>*>(b);
+    static DoublyLinkedList<T>* cast(Base* b) {
+        auto* ptr = dynamic_cast<DoublyLinkedList<T>*>(b);
         if (!ptr) {
-            throw std::runtime_error("Invalid Base type for LinkedListHelper");
+            throw std::runtime_error("Invalid Base type for DoublyLinkedListHelper");
         }
         return ptr;
     }
 
-    static const LinkedList<T>* cast(const Base& b) {
-        auto* ptr = dynamic_cast<const LinkedList<T>*>(&b);
+    static const DoublyLinkedList<T>* cast(const Base& b) {
+        auto* ptr = dynamic_cast<const DoublyLinkedList<T>*>(&b);
         if (!ptr) {
-            throw std::runtime_error("Invalid Base type for LinkedListHelper");
+            throw std::runtime_error("Invalid Base type for DoublyLinkedListHelper");
         }
         return ptr;
     }
 public:
     void print(const Base& data) override {
-        const LinkedList<T>* list = cast(data);
+        const DoublyLinkedList<T>* list = cast(data);
 
-        std::cout << "LINKED_LIST (size = " << list->size() << "):";
-        for (const Node<T>* node = list->get_head(); node != nullptr; node = node->next) {
+        std::cout << "DOUBLY_LINKED_LIST (size = " << list->size() << "):";
+        for (const DNode<T>* node = list->get_head(); node != nullptr; node = node->next) {
             std::cout << " " << node->value;
         }
         std::cout << std::endl;
     }
 
     void save(const char* filename, const Base& data) override {
-        const LinkedList<T>* list = cast(data);
+        const DoublyLinkedList<T>* list = cast(data);
 
         std::ofstream out(filename);
         if (!out.is_open()) {
             throw std::runtime_error("Failed to open file for saving");
         }
 
-        out << "LINKED_LIST" << std::endl;
+        out << "DOUBLY_LINKED_LIST" << std::endl;
         out << list->size() << std::endl;
-        for (const Node<T>* node = list->get_head(); node != nullptr; node = node->next) {
+        for (const DNode<T>* node = list->get_head(); node != nullptr; node = node->next) {
             out << node->value << std::endl;
         }
         out.close();
     }
 
     void load(const char* filename, Base* data) override {
-        LinkedList<T>* list = cast(data);
+        DoublyLinkedList<T>* list = cast(data);
 
         std::ifstream in(filename);
         if (!in.is_open()) {
@@ -299,7 +319,7 @@ public:
 
         char header[64];
         in.getline(header, 64);
-        if ((in.fail() && !in.eof()) || std::strcmp(header, "LINKED_LIST") != 0 ) {
+        if ((in.fail() && !in.eof()) || std::strcmp(header, "DOUBLY_LINKED_LIST") != 0 ) {
             throw std::runtime_error("Invalid file format");
         }
 
--- C++/src/main.cpp
+++ C++/src/main.cpp
@@ -1,14 +1,15 @@
 #include "array.h"
 #include "linked_list.h"
+#include "doubly_linked_list.h"
 
 int main(/*int argc, char* argv[]*/) {
-    auto list = LinkedList<uint64_t>();
+    auto list = DoublyLinkedList<uint64_t>();
     list.push_tail(12);
     list.push_tail(13);
     list.push_after(12, 222);
-    auto helper = LinkedListHelper<uint64_t>();
+    auto helper = DoublyLinkedListHelper<uint64_t>();
     helper.save("pizdec.txt", list);
-    auto list2 = LinkedList<uint64_t>();
+    auto list2 = DoublyLinkedList<uint64_t>();
     helper.load("pizdec.txt", &list2);
     helper.print(list2);
     return 0;
